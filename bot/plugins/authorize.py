f7343e3b31b75a664baad1b83a2610abda55cab2
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-03-30 15:30:46 +0530

    Update authorize.py

--- bot/plugins/authorize.py
+++ bot/plugins/authorize.py
@@ -43,7 +43,7 @@ async def _auth(client, message):
         text=Messages.AUTH_TEXT.format(auth_url),
         quote=True,
         reply_markup=InlineKeyboardMarkup(
-                  [[InlineKeyboardButton("Authorization URL", url=auth_url)]]
+                  [[InlineKeyboardButton("ගිණුම හා සම්බන්ධ වීමට මෙය ඔබන්න", url=auth_url)]]
               )
         )
     except Exception as e:
@@ -70,7 +70,7 @@ async def _token(client, message):
     if flow:
       try:
         user_id = message.from_user.id
-        sent_message = await message.reply_text("🕵️**Checking received code...**", quote=True)
+        sent_message = await message.reply_text("🕵️**කේතය පරික්ෂා කරමින්...**", quote=True)
         creds = flow.step2_exchange(message.text)
         gDriveDB._set(user_id, creds)
         LOGGER.info(f'AuthSuccess: {user_id}')
